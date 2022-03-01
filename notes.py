75cc147a8cab7912b7d42bfa6c2ced10f81b6d53
SWARNAVA DUTTA <swarnava.dutta97@gmail.com>
2022-03-01 23:57:31 +0530

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
@@ -10,6 +11,8 @@ window.title("Easy Notes")
 ids = []
 selected_index = 0
 file = None
+
+
 def onselect(evt):
     global selected_index
     # Note here that Tkinter passes an event object to onselect()
@@ -22,7 +25,7 @@ def onselect(evt):
 
 thisMenuBar = Menu(window)
 thisFileMenu = Menu(thisMenuBar, tearoff=0)
-label = tk.Label(window, text = "Created by Swarnava Dutta").pack()
+label = tk.Label(window, text="Created by Swarnava Dutta").pack()
 top_frame = tk.Frame(window)
 scroll_list = tk.Scrollbar(top_frame)
 scroll_list.pack(side=tk.RIGHT, fill=tk.Y)
@@ -31,40 +34,39 @@ list_notes = tk.Listbox(top_frame, height=15, width=40)
 list_notes.bind('<<ListboxSelect>>', onselect)
 list_notes.pack(side=tk.LEFT, fill=tk.Y, padx=(10, 0), pady=(10, 10))
 
-
-
-
 scroll_list.config(command=list_notes.yview)
-list_notes.config(yscrollcommand=scroll_list.set, cursor="hand2", background="#fff5e6", highlightbackground="grey", bd=0, selectbackground="#c9b922")
+list_notes.config(yscrollcommand=scroll_list.set, cursor="hand2", background="#fff5e6", highlightbackground="grey",
+                  bd=0, selectbackground="#c9b922")
 top_frame.pack(side=tk.TOP, padx=(0, 5))
 
-
 text_frame = tk.Frame(window)
-note_title = tk.Entry(text_frame, width=39, font = "Helvetica 13")
+note_title = tk.Entry(text_frame, width=39, font="Helvetica 13")
 note_title.insert(tk.END, "Title")
 note_title.config(background="#F4F6F7", highlightbackground="grey")
 note_title.pack(side=tk.TOP, pady=(0, 5), padx=(0, 10))
-def saveFile():
-    file = None
-    if file == None:
-        # Save as new file
-        file = asksaveasfilename(initialfile=note_title.get(),
-                                        defaultextension=".txt",
-                                        filetypes=[("Text Documents", "*.txt")])
-        if file == "":
-            file = None
-        else:
-            # Try to save the file
-            file = open(file, "w")
-            file.write(note_text.get(1.0, END))
-            file.close()
-            # Change the window title
-            window.title(os.path.basename(file) + " - Notepad")
+
+
+# def saveFile():
+#     file = None
+#     if file == None:
+#         # Save as new file
+#         file = asksaveasfilename(initialfile=note_title.get(),
+#                                  defaultextension=".txt",
+#                                  filetypes=[("Text Documents", "*.txt")])
+#         if file == "":
+#             file = None
+#         else:
+#             # Try to save the file
+#             file = open(file, "w")
+#             file.write(note_text.get(1.0, END))
+#             file.close()
+#             # Change the window title
+#             window.title(os.path.basename(file) + " - Notepad")
 
 
 scroll_text = tk.Scrollbar(text_frame)
 scroll_text.pack(side=tk.RIGHT, fill=tk.Y)
-note_text = tk.Text(text_frame, height=7, width=40, font = "Helvetica 13")
+note_text = tk.Text(text_frame, height=7, width=40, font="Helvetica 13")
 note_text.pack(side=tk.TOP, fill=tk.Y, padx=(5, 0), pady=(0, 5))
 note_text.tag_config("tag_your_message", foreground="blue")
 note_text.insert(tk.END, "Notes")
@@ -78,9 +80,10 @@ photo_add = tk.PhotoImage(file="add.gif")
 photo_edit = tk.PhotoImage(file="edit.gif")
 photo_delete = tk.PhotoImage(file="delete.gif")
 
-btn_save = tk.Button(button_frame, text="Add", command=lambda : SaveInLocalStorage(), image=photo_add)
-btn_edit = tk.Button(button_frame, text="Update", command=lambda : update_note(), state=tk.DISABLED, image=photo_edit)
-btn_delete = tk.Button(button_frame, text="Delete", command=lambda : delete_note(), state=tk.DISABLED, image=photo_delete)
+btn_save = tk.Button(button_frame, text="Add", command=lambda: SaveInDatabase(), image=photo_add)
+btn_edit = tk.Button(button_frame, text="Update", command=lambda: update_note(), state=tk.DISABLED, image=photo_edit)
+btn_delete = tk.Button(button_frame, text="Delete", command=lambda: delete_note(), state=tk.DISABLED,
+                       image=photo_delete)
 
 btn_save.grid(row=0, column=1)
 btn_edit.grid(row=0, column=2)
@@ -103,9 +106,9 @@ def db_create_table(conn):
     conn.database = "EasyNotes"
     mycursor = conn.cursor()
     query = "CREATE TABLE IF NOT EXISTS notes (" \
-          "id INT AUTO_INCREMENT PRIMARY KEY, " \
-          "title VARCHAR(255) NOT NULL, " \
-          "note VARCHAR(2000) NOT NULL)"
+            "id INT AUTO_INCREMENT PRIMARY KEY, " \
+            "title VARCHAR(255) NOT NULL, " \
+            "note VARCHAR(10000) NOT NULL)"
     mycursor.execute(query)
 
 
@@ -164,10 +167,11 @@ def init(conn):
         list_notes.insert(tk.END, note[1])
         ids.append(note[0])  # save the id
 
+
 init(conn)
 
 
-def SaveInLocalStorage():
+def SaveInDatabase():
     global conn
     title = note_title.get()
 
@@ -193,14 +197,14 @@ def SaveInLocalStorage():
         tk.messagebox.showerror(title="ERROR!!!", message="Note title already exist. Please choose a new title")
         return
 
-    MsgBox = tk.messagebox.askquestion('Save Note', 'Do you want to save your note in local storage also?',
-                                       icon='warning')
-    if MsgBox == 'yes':
-        save_note()
-        saveFile()
-    else:
-        save_note()
-        tk.messagebox.showinfo('Saved', 'Your note is saved in the database')
+    # MsgBox = tk.messagebox.askquestion('Save Note', 'Do you want to save your note in local storage also?',
+    #                                    icon='warning')
+    # if MsgBox == 'yes':
+    #     save_note()
+    #     saveFile()
+    # else:
+    save_note()
+    tk.messagebox.showinfo('Saved', 'Your note is saved in the database')
 
 
 # BUTTON CLICK FUNCTION STARTS
@@ -243,7 +247,7 @@ def update_note():
 
     result = tk.messagebox.askquestion("Update", "Are you sure you want to update?", icon='warning')
     if result == 'yes':
-    # save in database
+        # save in database
         db_update_note(conn, title, note, id)
         tk.messagebox.showerror(title="Update", message="Your Note is Updated")
         # update list_note
@@ -269,6 +273,7 @@ def delete_note():
     result = tk.messagebox.askquestion("Delete", "Are you sure you want to delete?", icon='warning')
 
     if result == 'yes':
+        
         # remove notes from db
         id = ids[selected_index]
         db_delete_note(conn, id)
@@ -295,5 +300,6 @@ def display_note(index, value):
 
     btn_delete.config(state=tk.NORMAL)
     btn_edit.config(state=tk.NORMAL)
+
 
 window.mainloop()
